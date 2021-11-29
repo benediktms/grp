0d5d3d6428cbe545eaa2a52d151ddfa424e67ab0
Benedikt Schnatterbeck <benediktms@hey.com>
2021-11-29 09:10:07 +0000

    Add initial eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,17 @@
 module.exports = {
-  extends: ["blitz"],
-}
+  extends: ['blitz'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  ignorePatterns: ['.eslintrc.js'],
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+  rules: {
+    curly: ['error', 'all'],
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      { varsIgnorePattern: '^_', argsIgnorePattern: '^_' },
+    ],
+    '@typescript-eslint/no-floating-promises': 'error',
+  },
+};
